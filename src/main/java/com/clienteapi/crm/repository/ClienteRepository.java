9554af88ac00887e52fc18f885229693b78a5b1b
isabelcristinapereira <isabelcristina214@outlook.com>
2021-12-16 19:58:36 -0300

    criado metodos de pesquisa

--- src/main/java/com/clienteapi/crm/repository/ClienteRepository.java
+++ src/main/java/com/clienteapi/crm/repository/ClienteRepository.java
@@ -1,5 +1,9 @@
 package com.clienteapi.crm.repository;
 
+
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import com.clienteapi.crm.model.Cliente;
 
 @Repository 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
+ // vamos usar o list e option o list quando quisermos pesquisar um monte de informação, instancia
+	Optional<Cliente> findById(Long id);
 }
